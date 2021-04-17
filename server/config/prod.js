b18a2481b45006650c8cd0b23a4a2933b18f3b77
jiho9505 <sun_days@naver.com>
2021-04-17 18:05:07 +0900

    Updated config

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,5 +3,9 @@ module.exports = {
     dialogFlowSessionID: process.env.dialogFlowSessionID ,
     dialogFlowSessionLanguageCode: process.env.dialogFlowSessionLanguageCode,
     googleClientEmail: process.env.googleClientEmail,
-    googlePrivateKey: process.env.googlePrivateKey
+    googlePrivateKey: process.env.googlePrivateKey,
+    mongoURI: process.env.mongoURI,
+    accessKeyId: process.env.accessKeyId,
+    secretAccessKey: process.env.secretAccessKey,
+    region: process.env.region
 }
