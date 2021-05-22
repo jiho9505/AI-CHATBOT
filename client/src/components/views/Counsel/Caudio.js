a8311feb4965661018c129efaf827df3926984a0
jiho9505 <sun_days@naver.com>
2021-05-22 12:04:15 +0900

    Deploy TEST

--- client/src/components/views/Counsel/Caudio.js
+++ client/src/components/views/Counsel/Caudio.js
@@ -2,10 +2,12 @@ import React from 'react'
 import audio from './output.mp3'
 
 function Caudio() {
+    console.log('p')
     return (
         <div>
             <audio style={{display:'none'}} src={audio} controls autoPlay/>
         </div>
+        
     )
 }
 
