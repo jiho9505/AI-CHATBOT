5b6d035ec4aaa20475afd10c5e987e0d9da9a703
jiho9505 <sun_days@naver.com>
2021-05-24 10:07:55 +0900

    Path check ...ing

--- server/routes/gs.js
+++ server/routes/gs.js
@@ -34,15 +34,15 @@ router.post('/tts', async (req, res) => {
     
     console.log('p',path.join(__dirname,'../../client/src/components/views/Counsel/output.mp3'))
     log(`__dirname`, __dirname);
-    log(`process.cwd()`, process.cwd());
-    log(`app_root_path`, app_root_path);
+    log(`process.cwd()`, path.join(process.cwd(),'client/src/components/views/Counsel/output.mp3'));
+    log(`app_root_path`, path.join(app_root_path,'client/src/components/views/Counsel/output.mp3'));
     // /app/client/src/components/views/Counsel/output.mp3
     // console.log('p',th)
     // C:\Users\문지호\Desktop\WebProject\Web_Capston\client\src\components\views\Counsel\output.mp3'
     // '
     // path: path.join(__dirname, 'path/to/.env')
     // path.join => path.posix.join
-    const outputFile = path.join(__dirname,'../../client/src/components/views/Counsel/test.mp3')
+    const outputFile = path.join(process.cwd(),'client/src/components/views/Counsel/output.mp3')
     // Performs the text-to-speech request
     const [response] = await client.synthesizeSpeech(request);
     // Write the binary audio content to a local file
