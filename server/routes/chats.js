80d870755514950802247be0e8b762a85940f105
jiho9505 <sun_days@naver.com>
2021-05-26 20:19:07 +0900

    Finish

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -19,7 +19,7 @@ router.post("/make", (req, res) => {
 
     Chats.findOne({ writer : req.body.writer } , (err,info)=>{
         if(err) return res.json({ success: false, message: 'Error 발생..' })
-        console.log('info',info)
+        
         if(info){
             Chats.updateOne({writer : req.body.writer}, {msg : req.body.msg}, (err) =>{
                 if (err) return res.json({ success: false, message: 'Error' });
@@ -36,37 +36,11 @@ router.post("/make", (req, res) => {
                 });
             })
         }
-        
-    //     if(msg){
-    //         Chats.insertMany({ writer : req.body._id }, () => {
-
-    //         })
-    //     }else{
-    //         const chat = new Chats(req.body);
-    //         chat.save((err, doc) => {                                                                                    
-    //             if (err) return res.json({ success: false, message: '중복된 E-mail이 있습니다' });
-    //             return res.status(200).json({
-    //                 success: true
-    //             });
-    // });
-    //     }
-    //     return res.json({
-    //         success: true,
-    //         msg: msg
-    //     });
-    // })
+    
     })
 });
 
-// router.post("/", (req, res) => {
-//     User.findOne({ nickname : req.body.nickname } , (err,userInfo)=>{
-//         if(err) return res.json({ success: false, message: 'Error 발생..' })
-//         if(userInfo) return res.json({ success: false, message: '중복된 닉네임이 있습니다' })
 
-        
-//     })
-    
-// });
 
 
 module.exports = router;
