c65dddae9eaeaaea06abf8b181f77d5a7152eec5
jiho9505 <sun_days@naver.com>
2021-05-26 14:47:44 +0900

    Refactoring ing...

--- client/src/_actions/user_actions.js
+++ client/src/_actions/user_actions.js
@@ -1,32 +1,5 @@
 import axios from 'axios';
-import {
-    LOGIN_USER,
-    REGISTER_USER,
-    AUTH_USER,
-    LOGOUT_USER,
-} from './types';
-
-const USER_SERVER = '/api/users';
-
-export function registerUser(dataToSubmit){
-    const request = axios.post(`${USER_SERVER}/register`,dataToSubmit)
-        .then(response => response.data);
-    
-    return {
-        type: REGISTER_USER,
-        payload: request
-    }
-}
-
-export function loginUser(dataToSubmit){
-    const request = axios.post(`${USER_SERVER}/login`,dataToSubmit)
-                .then(response => response.data);
-
-    return {
-        type: LOGIN_USER,
-        payload: request
-    }
-}
+import { AUTH_USER } from './types';
 
 export function auth(){
     const request = axios.get(`/api/users/auth`)
@@ -38,13 +11,4 @@ export function auth(){
     }
 }
 
-export function logoutUser(){
-    const request = axios.get(`${USER_SERVER}/logout`)
-    .then(response => response.data);
-
-    return {
-        type: LOGOUT_USER,
-        payload: request
-    }
-}
 
